adc87de2d0fa3df19719c11ac4453d8b6db96a33
“will <jodeelmoody@gmail.com>
2022-12-12 12:59:52 -0800

    poller test

--- service/poll/poller.py
+++ service/poll/poller.py
@@ -11,17 +11,27 @@ django.setup()
 
 # Import models from service_rest, here.
 # from service_rest.models import Something
+from service_rest.models import Inventory_vinVO
+
+def get_auto():
+    response = requests.get("http://inventory-api:8000/api/automobiles/")
+    content = json.loads(response.content)
+    for auto in content["autos"]:
+        Inventory_vinVO.objects.update_or_create(
+            import_href=auto["href"],
+            defaults={"vin":auto["vin"]},
+        )
 
 def poll():
     while True:
         print('Service poller polling for data')
         try:
-            # Write your polling logic, here
+            get_auto()
             pass
         except Exception as e:
             print(e, file=sys.stderr)
         time.sleep(60)
 
-
 if __name__ == "__main__":
     poll()
+
